03ef6e67401c7f9f595310c43811b17c0e93981d
JustaSqu1d <89910983+JustaSqu1d@users.noreply.github.com>
2025-02-23 11:12:38 -0800

    feat: add euic to default selected

--- dreepy-metrics/data.py
+++ dreepy-metrics/data.py
@@ -123,4 +123,4 @@ def get_events(show_future=False):
 def get_active_events():
     return ["baltimore2025", "dortmund2025", "joinville2025", "louisville2025", "lille2025",
             "gdansk2025", "laic2025", "sacramento2025", "stuttgart2025", "perth2025", "toronto2025", 
-            "birmingham2025", "riodejaneiro2025", "sanantonio2025", "merida2025"]
+            "birmingham2025", "riodejaneiro2025", "sanantonio2025", "merida2025", "euic2025"]
